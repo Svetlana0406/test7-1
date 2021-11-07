6dc124ed0a0e9746b22fc7adeef185965e1a8961
Svetlana <svetlana.samrinova@yandex.ru>
2021-11-07 22:03:44 +0300

    added test StatsServiceTest

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -5,16 +5,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
+    long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
 
     @Test
     void findMax() {
         StatsService service = new StatsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    void findMaxMonthlyIncomeAfterFeb() {
+        StatsService service = new StatsService();
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
 }
